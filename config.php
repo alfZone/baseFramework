347d56d1d9d445a093910e806f4cd61e3235a9c0
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2024-06-12 11:41:53 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,31 @@
+/*
+* @author alf
+ * @copyright 2024
+ */
+
+/* define - constantes. Pode-se utilizar echo _SERVER;  */
+define("_AUTORES", 'author name');
+define("_METAKEYS", 'key1, key2 ...');
+define("_SERVER", 'localhost');
+define("_SITE", 'Site name');
+
+define("_CAMINHO_TEMPLATE", 'relative template path');
+define("_CAMINHO_TEMPLATE_ADM", 'relative adm template path');
+define("_CAMINHO_ADM", 'relative adm files path');
+define("_CAMINHO_REAL", 'file system real path');
+
+define("_CAMINHO_LOGIN", "/public/login");
+define("_CAMINHO_BACKEND", "/public/admin");
+
+define("_BDUSER", 'db user');
+define("_BDPASS", 'db pass');
+define("_BD", 'db name');
+
+define("_LINGUA", 'pt');
+define("_APL", 'NET');
+define("_URL", 'https://galeria.esmonserrate.org/rede/');
+
+//Database credentiails
+define("_GOOGLEID", '436091069232-el1mistio1k5sa1if5sa8hadgaqt3b2q.apps.googleusercontent.com');
+
+?>
